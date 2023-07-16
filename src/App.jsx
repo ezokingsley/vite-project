dcaa140dd24cfc2671dc78c1fbfa1a2fc828b9b0
ezotech <ezokingsley53@gmail.com>
2023-07-16 01:37:39 +0100

    understanding html tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hi Guys i'm Ezo kingsley</h1>
+      <h2>Frontend Web-Developer</h2>
+      <h3>And more...</h3>
+
+      <h5>ezotech BIO</h5>
+    <p>Ezo Kingsley is a dynamic individual with a diverse range of talents and interests. Born and raised in a small     town,  also have passion for learning from a young age. 
+    Final year student in computer science of Federal University of Petroleum Resources Effurun, Ezo embarked on a journey as a frontend web developer on the 1st july, 2023 with <b>Greatness Abolade.</b>
+    Ezo kingsley skills encompass a wide range of programming languages, including HTML, CSS, JavaScript, and more. 
+    Beyond their expertise in frontend development, Ezo is a visionary and a problem solver.
+    </p>
+
+    <p> <b>TAGS:</b>
+      
+      tags are uesd to declare specific actions e.g forms,table 
+      formatting of text e.g paragraphy, h1 to h6
+      also used for creating different segment e.g header, nav, body etc...
+    </p>
+
+    <h6><b>OPENING & CLOSING TAGS:</b><br />
+    An opening tag begins a section of page content, and a closing tag ends it. For example, to markup a section of text as a paragraph, you would open the paragraph with an opening paragraph tag  and close it with a closing paragraph tag  (closing tags always proceed the element with a /).
+    <br />
+    E.g 
+    <h1>I'm a Frontend Developer...</h1>
+    <h2>I'm a Frontend Developer...</h2>
+    <h3>I'm a Frontend Developer...</h3>
+    <h4>I'm a Frontend Developer...</h4>
+    <h5>I'm a Frontend Developer...</h5>
+    <h6>I'm a Frontend Developer...</h6>
+    </h6>
+
+    <h3> <b>HTML:</b> <mark><i>Hyper</i> <em>text</em> markup language</mark> also known as the structure of a <ins>website</ins>
+    </h3>
+
+    <h4>There are no difference between <b>bold</b>and <strong>strong</strong> both perform thesame function</h4>
+
+    <h3>Glucose <br />C<sub>6</sub>H<sub>12</sub>0<sub>6</sub></h3>
+    <h3>oxide <br /> O <sup>2-</sup></h3>
+
     </>
   )
 }
